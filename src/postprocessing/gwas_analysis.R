5ff9a2b7ea8df38cc871577e564aaec65626a313
rbonazzola <rodbonazzola@gmail.com>
2024-01-17 17:59:01 +0000

    Modified Manhattan plot and fixed logical condition for file existence

--- src/postprocessing/gwas_analysis.R
+++ src/postprocessing/gwas_analysis.R
@@ -10,44 +10,85 @@ suppressPackageStartupMessages({
 
 setwd(system("git rev-parse --show-toplevel", intern = TRUE))
 
-parser <- ArgumentParser()
-parser$add_argument("--gwas_folder", nargs="+")
-parser$add_argument("--output_folder", default=NULL)
-parser$add_argument("--gwas_pattern", default="GWAS__{phenotype}", help="File pattern including the \"{phenotype}\" field, *without extensions*.")
-parser$add_argument("--phenotypes", nargs="+", default=sapply(0:7, function(x) paste0("z00", x)) )
-parser$add_argument("--title", default=FALSE, action="store_true")
-parser$add_argument("--cache_rds", action="store_true", default=FALSE)
-parser$add_argument("--overwrite_rds", action="store_true", default=FALSE)
-parser$add_argument("--qqplot_pooled", action="store_true", default=FALSE)
-parser$add_argument("--color_odd_chr", default="mediumblue")
-parser$add_argument("--color_even_chr", default="lightblue")
-parser$add_argument("--figure_type", default="eps")
-parser$add_argument("--debug", action="store_true", default=FALSE)
+fetch_args <- function() {
 
-args <- parser$parse_args()
-
+    parser <- ArgumentParser()
+    parser$add_argument("--gwas_folder", nargs="+")
+    parser$add_argument("--output_folder", default=NULL)
+    parser$add_argument("--gwas_pattern", default="GWAS__{phenotype}", help="File pattern including the \"{phenotype}\" field, *without extensions*.")
+    parser$add_argument("--phenotypes", nargs="+", default=sapply(0:7, function(x) paste0("z00", x)) )
+    parser$add_argument("--title", default=FALSE, action="store_true")
+    parser$add_argument("--cache_rds", action="store_true", default=FALSE)
+    parser$add_argument("--overwrite_rds", action="store_true", default=FALSE)
+    parser$add_argument("--no_manhattan", action="store_true", default=FALSE)
+    parser$add_argument("--no_qqplot", action="store_true", default=FALSE)
+    parser$add_argument("--no_summary", action="store_true", default=FALSE)
+    parser$add_argument("--qqplot_pooled", action="store_true", default=FALSE)
+    parser$add_argument("--color_odd_chr", default="mediumblue")
+    parser$add_argument("--color_even_chr", default="lightblue")
+    parser$add_argument("--figure_type", default="eps")
+    parser$add_argument("--no_axis_labels",  action="store_true", default=FALSE)
+    parser$add_argument("--debug", action="store_true", default=FALSE)
+    
+    args <- parser$parse_args()
+    args
+    
+}
 ####################################################################################################
 
 main <- function(args) {
   
     # TODO: put every file pattern into a configuration file
-    gwas_folder <- args$gwas_folder
-    output_dir <- ifelse(is.null(args$output_folder), args$gwas_folder, args$output_folder)
-
     plot_params <- list()
     plot_params$colors = c(args$color_odd_chr, args$color_even_chr)
+    plot_params$no_axis_labels = args$no_axis_labels
     # color1 <- "hotpink4"
     # color2 <- "palevioletred2"
     
-    # text files
-    gwas_fp <- file.path(gwas_folder, paste0(args$gwas_pattern, ".tsv"))
-    # gwas_fp_rds <- file.path(gwas_folder, paste0(args$gwas_pattern, ".rds"))
-    gwas_summary_fp <- file.path(output_dir, "summaries", paste0(args$gwas_pattern, "__regionwise_summary.tsv"))
+    which_plot <- list()
+    which_plot$manhattan <- !args$no_manhattan
+    which_plot$qqplot <- !args$no_qqplot
+    which_plot$summary <- !args$no_summary
     
     if (args$debug) setLevel("DEBUG")
+
+    gwas_folder <- args$gwas_folder
+    output_dir <- ifelse(is.null(args$output_folder), args$gwas_folder, args$output_folder)  
+    fp <- build_file_patterns(gwas_folder, output_dir)
+    
+    logging::logdebug(glue("Phenotypes to be processed: {args$phenotypes}"))
+    logging::logdebug(fp)
+    
+    for (run_id in args$gwas_folder) {
+      
+        # TOFIX: run_id is not what I want here.
+        logging::loginfo("Processing run with ID {run_id}" %>% glue)
+      
+        if ( !file.exists(glue::glue(fp$figs_dir))) { 
+          dir.create(glue::glue(fp$figs_dir), showWarnings = TRUE)
+        }
+        
+        pvals_pooled <- vector(length = 0)
+        
+        lapply(args$phenotypes, process_phenotype, file_patterns=fp, plot_params=plot_params, which_plot=which_plot)
+        
+        # POOLED PHENOTYPES' Q-Q PLOT
+        if (args$qqplot_pooled) {
+          logging::loginfo("Gathering all the associations ({length(pvals_pooled)}) into a single QQ-plot..." %>% glue)
+          plot_qqplot(pvals_pooled, ofile=glue(qqplot_all_fp))
+        }
+    }
+}
+
+build_file_patterns <- function(gwas_folder, output_folder, gwas_filepattern="GWAS_{phenotype}") {
+
+    # text files
+    gwas_fp <- file.path(gwas_folder, paste0(gwas_filepattern, ".tsv"))
+    # gwas_fp_rds <- file.path(gwas_folder, paste0(args$gwas_pattern, ".rds"))
+    gwas_summary_fp <- file.path(output_folder, "summaries", paste0(args$gwas_pattern, "__regionwise_summary.tsv"))
     
     # figures
-    figs_dir <- file.path(output_dir, "figures")
+    figs_dir <- file.path(output_folder, "figures")
     
     if (args$figure_type == "png") { manhattan_fp <- file.path(figs_dir, paste0(args$gwas_pattern, "__manhattan.png")) } 
     else if (args$figure_type == "eps") { manhattan_fp <- file.path(figs_dir, paste0(args$gwas_pattern, "__manhattan.eps")) }
@@ -61,29 +102,8 @@ main <- function(args) {
     fp$qqplot_all_fp <- qqplot_all_fp
     fp$gwas_summary_fp <- gwas_summary_fp
     fp$gwas_fp <- gwas_fp
-    
-    logging::logdebug(glue("Phenotypes to be processed: {args$phenotypes}"))
-    logging::logdebug(fp)
-    
-    for (run_id in args$gwas_folder) {
-      
-        # TOFIX: run_id is not what I want here.
-        logging::loginfo("Processing run with ID {run_id}" %>% glue)
-        
-        if ( !file.exists(glue::glue(figs_dir))) { 
-          dir.create(glue::glue(figs_dir))
-        }
-        
-        pvals_pooled <- vector(length = 0)
-        
-        lapply(args$phenotypes, process_phenotype, file_patterns=fp, plot_params=plot_params)
-        
-        # POOLED PHENOTYPES' Q-Q PLOT
-        if (args$qqplot_pooled) {
-          logging::loginfo("Gathering all the associations ({length(pvals_pooled)}) into a single QQ-plot..." %>% glue)
-          plot_qqplot(pvals_pooled, ofile=glue(qqplot_all_fp))
-        }
-    }
+    fp$figs_dir <- figs_dir
+    fp
 }
 
 # Process LD-independent genomic regions
@@ -116,14 +136,14 @@ regionwise_summary <- function(gwas_df, regions, ofile) {
 
 ####################################################################################################
 
-load_gwas_df <- function(file, overwrite_rds) {
+load_gwas_df <- function(file, overwrite_rds=FALSE, cache_rds=TRUE) {
   
     gwas_f <- file
     gwas_f_rds <- gsub("tsv", "rds", gwas_f)
     
-    if (!file.exists(gwas_f) && !file.exists(gwas_f_rds) || file.info(file)$size == 0) {
+    if (!file.exists(gwas_f) && !file.exists(gwas_f_rds) || (file.exists(gwas_f) && file.info(gwas_f)$size == 0)) {
         logging::logdebug(glue("File {file} does not exist or it is empty."))
-        next
+        return(NULL)
     }
     
     rds_log_flag <- TRUE
@@ -138,7 +158,7 @@ load_gwas_df <- function(file, overwrite_rds) {
     } else {
         logging::loginfo("Reading the GWAS file in text format: {gwas_f}." %>% glue)
         gwas_df <- read_tsv(gwas_f, col_names = TRUE)
-        if (args$cache_rds) {
+        if (cache_rds) {
           logging::loginfo(glue("Caching GWAS in RDS format."))
           saveRDS(gwas_df, gwas_f_rds)
         }
@@ -146,31 +166,48 @@ load_gwas_df <- function(file, overwrite_rds) {
     gwas_df %>% filter(!is.na(P) & P != 0)
 }
 
-plot_manhattan <- function(gwas_df, ofile, figure_type, colors) {
+plot_manhattan <- function(gwas_df, ofile, figure_type, colors, no_axis_labels=FALSE) {
   
-    if (figure_type == "eps") {
-      postscript(file=ofile, width = 12, height=4)
-    } else if (figure_type == "png") {
-      png(ofile, res=100, width = 3000, height = 1000)
-    }
-    
-    logging::loginfo(glue("Creating Manhattan plot at {ofile}"))
-    
-    gwas_df <- rbind(gwas_df %>% filter(P < 1e-3), gwas_df %>% filter(P < 1e-2 & P > 3e-3) %>% sample_frac(0.1), gwas_df %>% filter(P < 3e-2 & P > 1e-3) %>% sample_frac(0.5))
-    
-    pp <- qqman::manhattan(
-      gwas_df, 
-      # main=plot_title, 
-      cex = 0.75, cex.lab = 1, cex.axis = 1, 
-      genomewideline = -log10(1.5e-10),
-      suggestiveline = -log10(5e-8), 
-      col = colors,
-      ylim = c(1.5, 20)
-    )
-    
-    axis(2, at = 2*1:10)
-    dev.off()
+  # ymax is closest multiple of 5 
+  ymax <- 5 * (as.integer(max(-log10(gwas_df$P)) / 5) + 1)
   
+  if (figure_type == "eps") {
+    postscript(file=ofile, width = 12, height=4)
+  } else if (figure_type == "png") {
+    png(ofile, res=100, width = 3000, height = min(80 * ymax, 1200))
+  }
+  
+  # gwas_df <- rbind(gwas_df %>% filter(P < 1e-3), gwas_df %>% filter(P < 1e-2 & P > 3e-3) %>% sample_frac(0.1), gwas_df %>% filter(P < 3e-2 & P > 1e-3) %>% sample_frac(0.5))
+  # gwas_df
+  
+  if (no_axis_labels) {
+  
+      pp <- qqman::manhattan(
+        gwas_df, 
+        # main=plot_title, 
+        cex = 0.75, cex.axis = 2, xlab="", ylab="",
+        genomewideline = -log10(1.5e-10),
+        suggestiveline = -log10(5e-8), 
+        col = colors,
+        ylim = c(0, ymax)
+      )
+      
+  } else {
+    
+      pp <- qqman::manhattan(
+        gwas_df, 
+        # main=plot_title, 
+        chrlabs = 1:22,
+        cex = 0.75, cex.axis = 2,
+        genomewideline = -log10(1.5e-10),
+        suggestiveline = -log10(5e-8), 
+        col = colors,
+        ylim = c(0, ymax)
+      )
+      
+  }
+      
+  dev.off()
 }
 
 plot_qqplot <- function(pvals, ofile) {
@@ -179,7 +216,7 @@ plot_qqplot <- function(pvals, ofile) {
       logging::logerror("List of p-values is empty. Skipping Q-Q plot.")
     
     png(glue(ofile), res=100, width = 1000, height = 1000)
-    logging::loginfo(glue("Creating Q-Q plot at {ofile}"))
+    
     pp <- qqman::qq(pvals, 
         # main=plot_title, 
         cex.axis=2, 
@@ -190,26 +227,35 @@ plot_qqplot <- function(pvals, ofile) {
 
 ########################################################################
 
-process_phenotype <- function(phenotype, file_patterns, plot_params) {
-  
-    fp <- file_patterns
+process_phenotype <- function(phenotype, file_patterns, plot_params, which_plot) {
   
     logging::loginfo("Processing {phenotype}..." %>% glue)
-    
+
     logging::loginfo("Loading GWAS summary statistics...")
-    gwas_df <- load_gwas_df(file=glue(fp$gwas_fp), overwrite_rds = args$overwrite_rds)
+    gwas_df <- load_gwas_df(file=glue(file_patterns$gwas_fp), overwrite_rds = args$overwrite_rds, cache_rds = args$cache_rds)
+    if (is.null(gwas_df)) { return(NULL) }
     
-    plot_title <- ifelse(args$title, yes = "{run_id} - {phenotype}" %>% glue, no = "")
+    if (which_plot$manhattan) {
+      man_f <- glue(file_patterns$manhattan_fp)
+      logging::loginfo(glue("Creating Manhattan plot at {man_f}"))
+      plot_manhattan(gwas_df, ofile=man_f, figure_type = args$figure_type, colors=plot_params$colors, no_axis_labels=plot_params$no_axis_labels)
+    }
     
-    plot_manhattan(gwas_df, ofile=glue(fp$manhattan_fp), figure_type = args$figure_type, colors=plot_params$colors)
-    plot_qqplot(gwas_df$P, ofile=glue(fp$qqplot_fp))
+    if (which_plot$qqplot) {
+      qq_f <- glue(file_patterns$qqplot_fp)
+      logging::loginfo(glue("Creating Q-Q plot at {qq_f}"))
+      plot_qqplot(gwas_df$P, ofile=qq_f)
+    }
     
-    # Produce region-wise summary (best p-value per region)
-    # regions <- load_ld_indep_regions()
-    # regionwise_summary(gwas_df, regions, ofile=glue(fp$gwas_summary_fp))
+    if (which_plot$summary) {
+      # Produce region-wise summary (best p-value per region)
+      regions <- load_ld_indep_regions()
+      regionwise_summary(gwas_df, regions, ofile=glue(fp$gwas_summary_fp))
+    }
   
 }
 
 ########################################################################
 
+args <- fetch_args()
 main(args)
